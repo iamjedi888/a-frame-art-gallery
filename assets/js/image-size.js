e680b0b4941d8136665438a33ab95d670cc52838
iamJedi888 <117941178+iamjedi888@users.noreply.github.com>
2024-06-08 23:44:58 -0400

    improve

--- assets/js/image-size.js
+++ assets/js/image-size.js
@@ -1,22 +1,30 @@
 AFRAME.registerComponent('image-size', {
-    init: function () {
-        let el = this.el
-        let srcId = el.getAttribute('src')
-        let img = document.getElementById(srcId.replace('#', ''))
-        let frame = el.querySelector('.frame')
-        img.onload = function () {
-            let w = img.naturalWidth
-            let h = img.naturalHeight
-            let ratio = w / h
-            let newW = 1
-            let newH = newW / ratio
-            let padding = 0.25
-            el.setAttribute('height', newH)
-            el.setAttribute('width', newW)
-            if (frame) {
-                frame.setAttribute('height', newH + padding)
-                frame.setAttribute('depth', newW + padding)
-            }
+    init: function() {
+      const el = this.el;
+      const imgId = el.getAttribute('src').replace('#', '-image');
+      const imgEl = document.getElementById(imgId);
+      const frameEl = el.querySelector('.frame');
+
+      if (!imgEl) {
+        console.warn(`Image with id ${imgId} not found.`);
+        return;
+      }
+
+      imgEl.addEventListener('load', () => {
+        const width = imgEl.naturalWidth;
+        const height = imgEl.naturalHeight;
+        const aspectRatio = width / height;
+        const newWidth = 1;
+        const newHeight = newWidth / aspectRatio;
+        const padding = 0.25;
+
+        el.setAttribute('height', newHeight);
+        el.setAttribute('width', newWidth);
+
+        if (frameEl) {
+          frameEl.setAttribute('height', newHeight + padding);
+          frameEl.setAttribute('depth', newWidth + padding);
         }
+      });
     }
 });
